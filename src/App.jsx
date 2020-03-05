0e0f4df6a76eb960fdf3be2a6278ae5ed9f1a6c1
thothdragon <thothdragons@gmail.com>
2020-03-05 15:39:29 +0100

    Next page implements

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,39 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
 import DataContainer from './containers/DataContainer';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Container, Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
 
 function App() {
   return (
     <Container>
+      <header>
+        <Navbar bg="light" expand="lg">
+          <Navbar.Brand href="#home">Face... Palm !</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+            <Form inline>
+              <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+              <Button variant="outline-success">Search</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Navbar>
+      </header>
+      
       <BrowserRouter>
         <Switch>
           <Route exact path="/:id(\d+)" component={DataContainer} />
+          <Route exact path="/:id(\d+)/profile" component={DataContainer} />
         </Switch>
       </BrowserRouter>
     </Container>
--- src/containers/DataContainer.jsx
+++ src/containers/DataContainer.jsx
@@ -10,22 +10,33 @@ export default class DataContainer extends Component {
   }
 
   fetchData = () => {
-    const { id } = 0;
+    const { id } = this.props.match.params;
     let url = `https://randomuser.me/api/?page=${id}&results=10&seed=zqsd&nat=fr`;
     Axios.get(url)
       .then(response => {
-        this.setState({ data: response.data })
+        this.setState({ data: response.data });
       })
       .catch(error => console.log(error));
   }
 
   componentDidMount = () => {
     this.fetchData();
+    this.props.match.params.id = Number(this.props.match.params.id) + 1;
+  }
+
+  componentDidUpdate = (prevProps) => {
+    const { id } = this.props.match.params;
+
+    if (id !== prevProps.match.params.id) {
+      this.setState({ data: null });
+      this.fetchData();
+      this.props.match.params.id = Number(this.props.match.params.id) + 1;
+    }
   }
 
   render = () => {
     const { data } = this.state;
-    const { id } = 0;
+    // const { id } = 1;
 
     if (!data) {
       return (
@@ -41,17 +52,21 @@ export default class DataContainer extends Component {
     }
 
     if (data.results) {
+      console.log('LOG de DATA', data);
       return (
         <div>
-          {/* <h1>Page</h1> */}
-          <Button>Page suivante</Button>
           <ListGroup>
             {data.results.map((item, index) =>
               <ListGroup.Item>
-                {item.name.title} {item.name.first} {item.name.last}
+                <Link to={`/${this.props.match.params.id}/profile`}>
+                  {item.name.title} {item.name.first} {item.name.last}
+                </Link>
               </ListGroup.Item>
             )}
           </ListGroup>
+          <Link to={`/${this.props.match.params.id}`}>
+            <Button>Page suivante</Button>
+          </Link>
         </div>
       );
     }
