28354ae5b520b139911da3a0173575f206d9cf85
thothdragon <thothdragons@gmail.com>
2020-03-05 10:09:02 +0100

    Initialize project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
+import DataContainer from './containers/DataContainer';
 
 function App() {
   return (
     <Container>
       Votre code ici!
+      <DataContainer />
     </Container>
   );
 }
